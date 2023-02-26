5eb7a7851c01342e330aa20bc82ff08a9e5526bf
Nils Blessing <nils.blessing2911@gmail.com>
2023-02-26 21:23:39 +0100

    added body-parser

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
 
 dotenv.config()
 
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(parseInt(process.env.PORT), () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
+    console.log(`App listening on port ${process.env.PORT}`)
 })
